14d1b795a692f9a2d41265e517075841c887811e
marselyna <theresiachintya27@gmail.com>
2024-09-20 11:04:13 +0700

    respon json

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 const http = require('http');
 
 // buat server
-const server = http.createServer((req, res) => {res.writeHead(200, {'content-Type': 'text/html'});
-res.write('selamat pagi');
-res.end();});
+const server = http.createServer((req, res) => {
+    // res.writeHead(200, {'content-Type': 'text/html'});
+    //res.write('selamat pagi');
+
+res.writeHead(200, {'content.Type': 'application/json'});
+res.write(JSON.stringify({
+    'message' : 'selamat pagi',
+    'status' : 'success'
+}));
+res.end();
+});
 
 const port = 3001;
 const host = 'localhost';
